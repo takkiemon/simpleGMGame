d1d9d880f38176ea8c267948c6c6b854611217db
Tak man Wong <tak.man.wong@hva.nl>
2017-09-25 17:38:04 +0200

    tags

--- Assets/Scripts/Eat.cs
+++ Assets/Scripts/Eat.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Eat : MonoBehaviour {
 
 	public string tagtag; //named tagtag instead of tag to avoid any confusions with existing tag in Unity library
+	public Text Score;
 	public float increase;
+
+	int score = 0;
 
 	void OnTriggerEnter(Collider other) {
 		if(other.gameObject.tag == tagtag) {
 			transform.localScale += new Vector3(increase, increase, increase);
 			Destroy(other.gameObject);
+
+			score += 10;
+			Score.text = "SCORE: " + score;
 		}
 	}
 }
