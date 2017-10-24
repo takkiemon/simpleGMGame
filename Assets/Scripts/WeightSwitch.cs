004b83aa9f22dd0d0d04de024625669176918a1e
Tak man Wong <tak.man.wong@hva.nl>
2017-10-24 16:18:53 +0200

    ffffff

--- Assets/Scripts/WeightSwitch.cs
+++ Assets/Scripts/WeightSwitch.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class WeightSwitch : MonoBehaviour {
 
-    public Color colorStart = Color.red;
-    public Color colorEnd = Color.green;
+	public Color colorStart = Color(1f, 0f, 0f);
+	public Color colorEnd = Color(0f, 1f, 0f);
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player" && other.transform.localScale.x >= 1.5)
         {
-            GetComponent<Renderer>().material.color = Color.green;
+			GetComponent<Renderer>().material.color = colorEnd;
         }
     }
 }
